cad46c4d5437c0fcdc6a231583a61eb9c34b4261
EAHITechnology <45755982+EAHITechnology@users.noreply.github.com>
2020-12-07 23:48:21 +0800

    Create model.go

---
+++ golang/eredis/model.go
@@ -0,0 +1,14 @@
+package eredis
+
+type RedisInfo struct {
+	RedisName      string
+	Addr           string
+	MaxIdle        int
+	MaxActive      int
+	IdleTimeout    int64
+	ReadTimeout    int64
+	WriteTimeout   int64
+	ConnectTimeout int64
+	Password       string
+	Wait           int8
+}
